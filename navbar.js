e7339f8be5fc26c3f09a983415fdde519a72589d
nathaphon39206 <nathaphon39206@gmail.com>
2023-09-09 11:20:18 +0700

    first commit

---
+++ navbar.js
@@ -0,0 +1,10 @@
+
+function openMenu(){
+  document.getElementById('side-menu').style.width='250px';
+  // document.getElementById('main').style.marginLeft='250px';
+}
+
+function closeMenu(){
+  document.getElementById('side-menu').style.width='0px';
+  // document.getElementById('main').style.marginLeft='0px';
+}
